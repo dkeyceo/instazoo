2c39a49d6363a61c6c51c05973c0c79f0c59fcc2
dkeyceo <dkeyceo@gmail.com>
2023-06-21 09:31:34 +0300

    Added jpa annotations to entities and generated tables in database

--- zoo-backend/src/main/java/com/dkey/zoo/entities/Post.java
+++ zoo-backend/src/main/java/com/dkey/zoo/entities/Post.java
@@ -1,25 +1,36 @@
 package com.dkey.zoo.entities;
 
 import com.dkey.zoo.entities.enums.ERole;
+import lombok.Data;
 
-import javax.persistence.PrePersist;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@Data
+@Entity
 public class Post {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String caption;
     private String location;
     private Integer likes;
 
+    @Column
+    @ElementCollection(targetClass = String.class)
     private Set<String> likedUsers = new HashSet<>();
+    @ManyToOne(fetch = FetchType.LAZY)
     private User user;
+    @OneToMany(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER,
+    mappedBy = "post", orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
+    @Column(updatable = false)
     private LocalDateTime createdDate;
 
     @PrePersist
