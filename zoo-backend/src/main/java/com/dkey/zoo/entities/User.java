182ba55a645801f0576e35cb592a9ca75531cd0f
dkeyceo <dkeyceo@gmail.com>
2023-06-21 16:07:52 +0300

    Implemented UserDetails interface in User entity

--- zoo-backend/src/main/java/com/dkey/zoo/entities/User.java
+++ zoo-backend/src/main/java/com/dkey/zoo/entities/User.java
@@ -8,12 +8,15 @@ import com.dkey.zoo.entities.enums.ERole;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 
 @Data
 @Entity
-public class User {
+public class User implements UserDetails {
+
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -42,9 +45,43 @@ public class User {
 
 	@Transient
 	private Collection<? extends GrantedAuthority> authorities;
+
+	public User(){}
+
+	public User(Long id, String username, String email, String password, Collection<? extends GrantedAuthority> authorities) {
+		this.id = id;
+		this.username = username;
+		this.email = email;
+		this.password = password;
+		this.authorities = authorities;
+	}
+
 	@PrePersist
 	protected void onCreate() {
 		this.createdDate = LocalDateTime.now();
 	}
 
+	@Override
+	public String getPassword() {
+		return this.password;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
