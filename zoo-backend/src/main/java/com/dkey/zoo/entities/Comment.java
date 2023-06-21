d7cfeb6d7782ca4cbaae53a2d4445fa62fef1d29
dkeyceo <dkeyceo@gmail.com>
2023-06-21 09:01:19 +0300

    Described ImageModel and Comment entities

--- zoo-backend/src/main/java/com/dkey/zoo/entities/Comment.java
+++ zoo-backend/src/main/java/com/dkey/zoo/entities/Comment.java
@@ -1,5 +1,21 @@
 package com.dkey.zoo.entities;
 
+import javax.persistence.PrePersist;
+import java.time.LocalDateTime;
+
 public class Comment {
+    private Long id;
+    private Post post;
+    private String username;
+    private Long userId;
+    private String message;
+
+    private LocalDateTime createdDate;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdDate = LocalDateTime.now();
+    }
+
 
 }
--- zoo-backend/src/main/java/com/dkey/zoo/entities/ImageModel.java
+++ zoo-backend/src/main/java/com/dkey/zoo/entities/ImageModel.java
@@ -2,4 +2,9 @@ package com.dkey.zoo.entities;
 
 public class ImageModel {
 
+    private Long id;
+    private String name;
+    private byte[] imageBytes;
+    private Long userId;
+    private Long postId;
 }
