eea0030b757ec5aa3674338413bf863d2255a291
dkeyceo <dkeyceo@gmail.com>
2023-06-25 20:27:50 +0300

    Added UserDTO, and updateUser method in UserService

--- zoo-backend/src/main/java/com/dkey/zoo/service/UserService.java
+++ zoo-backend/src/main/java/com/dkey/zoo/service/UserService.java
@@ -1,11 +1,15 @@
 package com.dkey.zoo.service;
 
+import java.security.Principal;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.dkey.zoo.dto.UserDTO;
 import com.dkey.zoo.entities.User;
 import com.dkey.zoo.entities.enums.ERole;
 import com.dkey.zoo.exceptions.UserExistException;
@@ -41,5 +45,21 @@ public class UserService {
 			LOG.error("Error during registration {}", ex.getMessage());
 			throw new UserExistException("The user "+ user.getUsername() + " already exists. Please check creds");
 		}
+	}
+	public User updateUser(UserDTO userDTO, Principal principal) {
+		User user = getUserByPrincipal(principal);
+		user.setName(userDTO.getFirstname());
+		user.setLastname(userDTO.getLastname());
+		user.setBio(userDTO.getBio());
+		return userRepository.save(user);
+	}
+	
+	public User getCurrentUser(Principal principal) {
+		return getUserByPrincipal(principal);
+	}
+	private User getUserByPrincipal(Principal principal) {
+		String username = principal.getName();
+		return userRepository.findUserByUsername(username)
+				.orElseThrow(()-> new UsernameNotFoundException("Username not found with username "+ username));
 	}
 }
