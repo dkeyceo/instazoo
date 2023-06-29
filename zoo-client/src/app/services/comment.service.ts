9adfa26c27818c7f8efc6a0357f9be668ee0a2e7
dkeyceo <dkeyceo@gmail.com>
2023-06-29 22:14:09 +0300

    Implemented commentService

--- zoo-client/src/app/services/comment.service.ts
+++ zoo-client/src/app/services/comment.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const COMMENT_API = 'http://localhost:8080/api/comment/'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  addCommentToPost(postId: number, message: string): Observable<any>{
+    return this.httpClient.post(COMMENT_API + postId + '/create', {
+      message: message
+    });
+  }
+
+  getCommentsToPost(postId: number): Observable<any> {
+    return this.httpClient.get(COMMENT_API + postId + '/all');
+  }
+
+  delete(commentId: number): Observable<any> {
+    return this.httpClient.post(COMMENT_API + commentId + '/delete', null);
+  }
+
 }
