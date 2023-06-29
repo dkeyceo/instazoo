2e0d3dcd17d94deb00f1253608aba19cc8d19ed1
dkeyceo <dkeyceo@gmail.com>
2023-06-29 21:47:14 +0300

    Implemented UserService

--- zoo-client/src/app/services/user.service.ts
+++ zoo-client/src/app/services/user.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/User';
+
+const USER_API = 'http://localhost:8080/api/user/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getUserById(id: number): Observable<User>{
+    return this.httpClient.get<User>(USER_API+id);
+  }
+
+  getCurrentUser(): Observable<User> {
+    return this.httpClient.get<User>(USER_API);
+  }
+
+  updateUser(user: User): Observable<User> {
+    return this.httpClient.post<User>(USER_API+'update', user);
+  }
+
 }
