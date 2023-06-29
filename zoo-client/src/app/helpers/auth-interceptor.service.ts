ab901f72b3bd2b4f371fc4d38a31e33b88781d5b
dkeyceo <dkeyceo@gmail.com>
2023-06-29 21:37:57 +0300

    Implemented TokenStorageService and AuthInterceptor

--- zoo-client/src/app/helpers/auth-interceptor.service.ts
+++ zoo-client/src/app/helpers/auth-interceptor.service.ts
@@ -1,9 +1,28 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TokenStorageService } from '../services/token-storage.service';
+
+const TOKEN_HEADER_KEY = 'Authorization';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthInterceptorService {
+export class AuthInterceptorService implements HttpInterceptor{
 
-  constructor() { }
+  constructor(private tokenStorageService: TokenStorageService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let authRequest = req;
+    const token = this.tokenStorageService.getToken();
+    if(token != null){
+      authRequest = req.clone({headers: req.headers.set(
+        TOKEN_HEADER_KEY, token
+      )});
+    }
+    return next.handle(authRequest);
+  }
+
+
 }
+
--- zoo-client/src/app/services/token-storage.service.ts
+++ zoo-client/src/app/services/token-storage.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/User';
+
+const TOKEN_KEY = 'auth-token';
+const USER_KEY  = 'auth-user';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +10,25 @@ import { Injectable } from '@angular/core';
 export class TokenStorageService {
 
   constructor() { }
+
+  public saveToken(token: string): void {
+    window.sessionStorage.removeItem(TOKEN_KEY);
+    window.sessionStorage.setItem(TOKEN_KEY, token);
+  }
+  public getToken() {
+    return sessionStorage.getItem(TOKEN_KEY);
+  }
+
+  public saveUser(user: User) {
+    window.sessionStorage.removeItem(USER_KEY);
+    window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+  public getUser() {
+    return sessionStorage.getItem(USER_KEY);
+  }
+  logOut(){
+    window.sessionStorage.clear();
+    window.location.reload()
+  }
+
 }
