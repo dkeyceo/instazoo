6d74cf0c50e2f306d075c62f392c0c676519ae9a
dkeyceo <dkeyceo@gmail.com>
2023-06-30 18:15:09 +0300

    Fixed some bugs about angular material, descibed and implemented login component (logic and view)

--- zoo-client/src/app/auth/login/login.component.ts
+++ zoo-client/src/app/auth/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { NotificationService } from 'src/app/services/notification.service';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public formGroup: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private authService: AuthService,
+    private tokenStorageService: TokenStorageService,
+    private notificationService: NotificationService,
+    private router: Router,
+    private fb: FormBuilder) {
+    if (this.tokenStorageService.getUser()) {
+      this.router.navigate(['/main'])
+    }
   }
 
+  ngOnInit(): void {
+    this.formGroup = this.createLoginForm()
+  }
+
+  createLoginForm(): FormGroup {
+    return this.fb.group({
+      username: ['', Validators.compose([Validators.required])],
+      password: ['', Validators.compose([Validators.required])]
+    })
+  }
+
+  submit(){
+    this.authService.login({
+      username: this.formGroup.value.username,
+      password: this.formGroup.value.password,
+    }).subscribe(data => {
+      console.log(data);
+      this.tokenStorageService.saveToken(data.token);
+      this.tokenStorageService.saveUser(data);
+      this.notificationService.showSnackBar('Successfully logged in');
+      this.router.navigate(['/']);
+      window.location.reload();
+    }, error => {
+      console.log(error);
+      this.notificationService.showSnackBar(error.message);
+    });
+  }
+  
 }
