faefaf2ed4f7180f1b3feaf814116c2e934c2f30
dkeyceo <dkeyceo@gmail.com>
2023-07-02 14:43:23 +0300

    Implemented edit-user component

--- zoo-client/src/app/user/edit-user/edit-user.component.ts
+++ zoo-client/src/app/user/edit-user/edit-user.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NotificationService } from 'src/app/services/notification.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-edit-user',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditUserComponent implements OnInit {
 
-  constructor() { }
+  public profileEditForm: FormGroup;
+
+  constructor(private dialogRef: MatDialogRef<EditUserComponent>,
+    private fb: FormBuilder,
+    private notificationSevice: NotificationService,
+    @Inject(MAT_DIALOG_DATA) public data,
+    private userService: UserService) { }
 
   ngOnInit(): void {
+    this.profileEditForm = this.createProfileForm();
   }
 
+  createProfileForm(): FormGroup {
+    return this.fb.group({
+      firstName: [
+        this.data.user.firstname,
+        Validators.compose([Validators.required])
+      ],
+      lastName: [
+        this.data.user.lastname,
+        Validators.compose([Validators.required])
+      ],
+      bio: [
+        this.data.user.bio,
+        Validators.compose([Validators.required])
+      ]
+    });
+  }
+
+  submit(){
+    this.userService.updateUser(this.updateUser())
+    .subscribe(() => {
+      this.notificationSevice.showSnackBar('User updated successfully');
+      this.dialogRef.close();
+    });
+  }
+
+  private updateUser() {
+    this.data.user.firstname = this.profileEditForm.value.firstName;
+    this.data.user.lastname = this.profileEditForm.value.lastName;
+    this.data.user.bio = this.profileEditForm.value.bio;
+    return this.data.user;
+  }
+  closeDialog(){
+    this.dialogRef.close();
+  }
 }
